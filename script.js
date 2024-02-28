e97d5c04a09c4b31b31c128db06ff7dc4b3e83dd
Juani-tech <jperezdichiazza@gmail.com>
2024-02-27 22:33:50 -0300

    dialog form style

--- script.js
+++ script.js
@@ -55,11 +55,10 @@ function createPlayer(name, symbol) {
         gameBoard.printGameBoard();
         displayGameBoard.updateGameBoard();
     }
+
     return { userMove, symbol, name}
 }
 
-const player1 = createPlayer("Player1", "X");
-const player2 = createPlayer("Player2", "O");
 
 
 function checkIfWon(gameBoard, player){
@@ -84,41 +83,59 @@ function checkIfWon(gameBoard, player){
 
 
 const game = (function() {
-    let currentPlayer = player1;
-    const startGame = () => {
-        currentPlayer = player1;
+    const startGame= () => {
+        const dialog = document.querySelector('dialog');
+        const form = document.getElementById("player-form");
+        const submitButton = document.getElementById("submit-button");
+        let player1, player2;
+        submitButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            const player1Name = document.getElementById("player-one-name").value;
+            const player2Name = document.getElementById("player-two-name").value;
+            player1 = createPlayer(player1Name, "X");
+            player2 = createPlayer(player2Name, "O");
+            dialog.close();
+            form.reset();
+        });
+        dialog.showModal();
+        return {player1, player2};
     }
+
     const checkValidMove = (x, y) => {
         if (gameBoard.getPosition(x, y) === "") {
             return true;
         }
         return false;
     }
-    const changePlayer = () => {
-        currentPlayer = (currentPlayer === player1) ? player2 : player1;
-    }
-    const getCurrentPlayer = () => {return currentPlayer;}
+    // const changePlayer = () => {
+    //     currentPlayer = (currentPlayer === player1) ? player2 : player1;
+    // }
 
     const resetGame = () => {   
         gameBoard.resetGameboard();
         displayGameBoard.updateGameBoard();
-        currentPlayer = player1;
+        // currentPlayer = player1;
     }
     
     const playGame = () => {
+        const {player1, player2} = startGame();
+        console.log(player1, player2);
+        const currentPlayer = player1;
+
         const cells = document.querySelectorAll('.cell');
         cells.forEach((cell, index) => {
             cell.addEventListener('click', () => {
                 const row = Math.floor(index / 3);
                 const column = index % 3;
-                if (game.checkValidMove(row, column)) {
-                    // const user = 
+                if (checkValidMove(row, column)) {
                     currentPlayer.userMove(currentPlayer, row, column);
                     if (checkIfWon(gameBoard, currentPlayer)) {
                         alert(`${currentPlayer.name} wins!`);
                         resetGame();
                     }
-                    game.changePlayer();
+                    currentPlayer = (currentPlayer === player1) ? player2 : player1;
+
+                    // changePlayer();
                 } else if (gameBoard.isFull()) {
                     alert("It's a tie!");
                     resetGame();
@@ -129,7 +146,5 @@ const game = (function() {
 
     return {playGame};
 })();
-
-
 
 game.playGame();
